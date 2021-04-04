a5fdd99e870511811bc4d6980e0ecbbbe8e2028f
pak11273 <pak11273@gmail.com>
2021-04-04 14:35:21 -0500

    formats 404 page

--- src/components/pages/NotFound/NotFoundPage.js
+++ src/components/pages/NotFound/NotFoundPage.js
@@ -1,15 +1,18 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
+import { StyledDiv } from '../SplashPage/styled';
 import { Styles } from './styled';
 
 const NotFoundPage = () => {
   return (
-    <Styles>
-      <h4>404 Page Not Found</h4>
-      <Link to="/">
-        <button>Back To Home</button>
-      </Link>
-    </Styles>
+    <StyledDiv>
+      <Styles>
+        <h4>404 Page Not Found</h4>
+        <Link to="/">
+          <button>Back To Home</button>
+        </Link>
+      </Styles>
+    </StyledDiv>
   );
 };
 
--- src/components/pages/NotFound/styled.js
+++ src/components/pages/NotFound/styled.js
@@ -1,18 +1,20 @@
 import styled from 'styled-components';
 
 export const Styles = styled.div`
+  min-height: 100vh;
   display: flex;
   flex-flow: column wrap;
   justify-content: center;
+  align-items: center;
   margin: 0 auto;
   max-width: 240px;
 
   h4 {
+    text-align: center;
     margin-bottom: 20px;
   }
 
   a {
-    align-self: center;
     margin: 0 auto;
   }
 
@@ -20,5 +22,9 @@ export const Styles = styled.div`
     &:hover {
       cursor: pointer;
     }
+    max-width: 100px;
+    border-radius: 5px;
+    text-align: center;
+    margin: auto 28%;
   }
 `;
